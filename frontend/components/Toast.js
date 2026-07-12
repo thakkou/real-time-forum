33be2030af87482480ef04d487437d87d7e40ca1
hamzaerrhh <hmzaeer@gmail.com>
2026-07-12 13:44:26 +0100

    sanitize the toast message

--- frontend/components/Toast.js
+++ frontend/components/Toast.js
@@ -1,4 +1,12 @@
+import {sanitize} from "../scripts/helpers.js"
 export const Toast = (message, type = "info", title = "") => {
+    const MAX_LENGTH = 25;
+message=sanitize(message)
+    const truncatedMessage =
+        message.length > MAX_LENGTH
+            ? message.slice(0, MAX_LENGTH) + "..."
+            : message;
+
     const toast = document.createElement("div");
     toast.className = `toast ${type}`;
 
@@ -7,11 +15,11 @@ export const Toast = (message, type = "info", title = "") => {
             <div class="toast-header">
                 <div class="toast-title">${title}</div>
             </div>
-            <div class="toast-message">${message}</div>
+            <div class="toast-message">${truncatedMessage}</div>
         `;
     } else {
         toast.innerHTML = `
-            <div class="toast-message">${message}</div>
+            <div class="toast-message">${truncatedMessage}</div>
         `;
     }
 
